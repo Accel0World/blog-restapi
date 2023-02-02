aefbfaae39a605a1ea24eae242afa63be8b6c8f2
Mohamed-Ach <mouhamed2015nasir@gmail.com>
2023-02-02 18:22:29 +0100

    further refinements

--- api/app/config.py
+++ api/app/config.py
@@ -1,20 +1,19 @@
 from pydantic import BaseSettings, PostgresDsn
 
+
 class Settings(BaseSettings):
 
-    # postgres_user: str
-    # postgres_password: str
-    # postgres_db: str
-    # postgres_host: str
-    # postgres_port: int
+    postgres_user: str = "PanDoRa_s-AcToR"
+    postgres_password: str = "nacxc5&EB&pSz__128"
+    postgres_db: str = "blog_db"
+    postgres_host: str = "postgres"
+    postgres_port: int = 5432
 
-    postgres_dsn: PostgresDsn
+    postgres_dsn: PostgresDsn = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+        pass
+        # pass for now!!!
 
 
 settings = Settings()
-
-print(settings.postgres_dsn)
